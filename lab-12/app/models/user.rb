504dc2b7a8ec187450918080caff4382a0121746
Artem Arkhipov <arhipartyom@yandex.ru>
2023-12-23 12:28:38 +0300

    all labs

--- lab-12/app/models/user.rb
+++
@@ -1,7 +0,0 @@
-# frozen_string_literal: true
-
-class User < ApplicationRecord
-  has_many :calcs
-
-  has_secure_password
-end
--- lab-12/db/migrate/4_add_index_user.rb
+++
@@ -1,8 +0,0 @@
-# frozen_string_literal: true
-
-class AddIndexUser < ActiveRecord::Migration[7.0]
-  def change
-    add_index :users, :username, unique: true
-    # add_index :users, :username
-  end
-end
